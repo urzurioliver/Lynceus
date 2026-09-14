3ffe00ba0e364b797d4decce0be019ab2c07860e
urzurioliver <urzurioliver@gmail.com>
2026-09-14 14:27:43 -0300

    algo de 5005

--- receptor.py
+++ receptor.py
@@ -1,4 +1,5 @@
 import socket
+import numpy as np
 
 # Configuración del socket UDP
 UDP_IP = "0.0.0.0"  # Escucha en todas las interfaces de red locales
@@ -8,13 +9,21 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 print(f"Escuchando datos CSI en el puerto {UDP_PORT} (UDP)...")
-
-while True:
+try:
+    while True:
     # Recibe hasta 4096 bytes por paquete
-    data, addr = sock.recvfrom(4096)
+        data, addr = sock.recvfrom(4096)
     
     # 'data' contiene los bytes crudos del payload CSI
-    print(f"Paquete recibido desde {addr} - Tamaño: {len(data)} bytes")
+        print(f"Paquete recibido desde {addr} - Tamaño: {len(data)} bytes")
     
     # Aquí irá tu lógica para parsear la matriz de amplitud y fase
     # ej: csi_matrix = parse_csi_payload(data)
+    #corro con python receptor.py
+        data, addr = sock.recvfrom(4096)
+        raw_array = np.frombuffer(data, dtype=np.int8)
+        real=raw_array[0::2]
+        imag=raw_array[1::2]
+        csi_complejo = real +1j * imag
+        amplitud = np.abs(csi_complejo)
+            
